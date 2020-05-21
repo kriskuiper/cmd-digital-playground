088a6826230ab6476d51250c997adc60fc16e627
Kris <kriskuiper74@gmail.com>
2020-05-21 09:42:45 +0200

    feat: add environment and previewKey to site data

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -1,12 +1,15 @@
 const Storyblok = require('../../lib/storyblok-instance')
 
 module.exports = async () => {
-  const version = process.env.ELEVENTY_ENV === 'production' ? 'published' : 'draft'
+  const env = process.env.ELEVENTY_ENV
+  const version = env === 'production' ? 'published' : 'draft'
 
   const result = await Storyblok.get('cdn/stories', { version })
   const { stories = [] } = result && result.data
 
   return {
+    env,
+    previewKey: env !== 'production' && process.env.STORYBLOK_PREVIEW_KEY,
     stories: stories.map(story => {
       return {
         content: story.content.content,
