67cf15de1ace511daf9edf8e51074c3c53389f54
Kris <kriskuiper74@gmail.com>
2020-05-21 09:49:37 +0200

    feat: add client-side storyblok change handler

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,9 @@
-console.log('🙌 Working!')
+// Handle Storyblok published and change events by reloading
+// the page. You can find the docs here: https://www.storyblok.com/docs/Guides/storyblok-latest-js
+if (storyblok) {
+  storyblok.on(['published', 'change'], (event) => {
+    if (!event.slugChanged) {
+      location.reload(true)
+    }
+  })
+}
