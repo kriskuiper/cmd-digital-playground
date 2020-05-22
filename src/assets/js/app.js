3774234a049432f3863f0e0a9a0eee0f65a5b159
Kris <kriskuiper74@gmail.com>
2020-05-22 14:00:54 +0200

    feat(preview): hijack the page using JS if storyblok is available

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,20 @@
+import preview from './preview'
+
 // Handle Storyblok published and change events by reloading
 // the page. You can find the docs here: https://www.storyblok.com/docs/Guides/storyblok-latest-js
 if (storyblok) {
-  storyblok.on(['published', 'change'], (event) => {
+  fetch(`/api/get-modular-content?path=${location.pathname}`)
+    .then(response => response.json())
+    .then(components => {
+      preview.render(components)
+    })
+
+  storyblok.on(['published', 'change'], async (event) => {
     if (!event.slugChanged) {
-      location.reload(true)
+      const response = await fetch(`/api/get-modular-content?path=${location.pathname}`)
+      const components = await response.json()
+
+      preview.render(components)
     }
   })
 }
