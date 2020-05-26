9d0c95a98076a8bc45c7fb61463000bc52c34ad3
Kris <kriskuiper74@gmail.com>
2020-05-26 14:33:33 +0200

    feat: change getting the data

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -5,18 +5,37 @@ module.exports = async () => {
   const version = env === 'production' ? 'published' : 'draft'
 
   const result = await Storyblok.get('cdn/stories', { version })
-  const { stories = [] } = result && result.data
+  const { stories = [] } = result.data
 
   return {
     env,
     previewKey: env !== 'production' && process.env.STORYBLOK_PREVIEW_KEY,
-    stories: stories.map(story => {
-      return {
-        content: story.content.content,
-        title: story.name,
-        full_slug: story.full_slug,
-        slug: story.slug
-      }
-    })
+    navigation: getNavigationData(stories),
+    stories: getPagesData(stories)
   }
+}
+
+function getNavigationData(stories) {
+  return stories.reduce((navigation, currentStory) => {
+    if (currentStory.full_slug.includes('main-menu')) {
+      navigation.mainMenu = currentStory
+
+      return navigation
+    }
+
+    return navigation
+  }, {})
+}
+
+function getPagesData(stories) {
+  return stories
+    .filter(story => {
+    return story.full_slug.includes('pages')
+    })
+    .map(story => ({
+      content: story.content.content,
+      title: story.name,
+      slug: story.slug,
+      full_slug: story.full_slug
+    }))
 }
