cdfbc87acedbf65b5413c441f15ef808978b2d33
gijslaarman <gijslaarman@gmail.com>
2020-05-27 14:17:34 +0200

    feat: create slug from component's name on storyblok

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -30,6 +30,7 @@ function getNavigationData(stories) {
 
 function addComponentSlug(content) {
   return content.map(contentBlock => {
+    // Make a slug from the component name, uses slugify.
     // Input: "Test component"
     contentBlock.componentSlug = slugify(contentBlock.component, { lower: true }) // test-component
     return contentBlock
