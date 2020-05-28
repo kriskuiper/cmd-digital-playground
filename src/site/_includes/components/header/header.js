4f3f224527a5659422dc86141c228fd8bc1210af
gijslaarman <gijslaarman@gmail.com>
2020-05-28 13:54:28 +0200

    responsive header with progressive enhancement

--- src/site/_includes/components/header/header.js
+++ src/site/_includes/components/header/header.js
@@ -1,1 +1,28 @@
-console.log('Yo ik ben header')
+const hdr = (function() {
+  const header = document.querySelector('.main-menu')
+  const nav = header.querySelector('nav')
+  const button = header.querySelector('button')
+  const initialHeight = header.clientHeight
+  let open = false
+
+  function toggleMenu() {
+    console.log(open);
+    header.classList.toggle('open')
+
+    if (open) {
+      // Remove height on header
+      header.removeAttribute('style')
+      button.innerText = 'Menu'
+      open = false
+    } else {
+      // Add height style on header
+      header.style.height = initialHeight + nav.clientHeight + 'px'
+      button.innerText = 'Sluiten'
+      open = true
+    }
+  }
+
+  return button.addEventListener('click', toggleMenu)
+})()
+
+export default hdr
