95b61aad31872d8c9427e793e5a21b31213798eb
gijslaarman <gijslaarman@gmail.com>
2020-06-02 10:27:05 +0200

    refactor: split up the image of the icon in the header

--- src/site/_includes/components/header/header.js
+++ src/site/_includes/components/header/header.js
@@ -1,28 +1,28 @@
-const hdr = (function() {
+export default (() => {
   const header = document.querySelector('.main-menu')
-  const nav = header.querySelector('nav')
-  const button = header.querySelector('button')
-  const initialHeight = header.clientHeight
-  let open = false
 
-  function toggleMenu() {
-    console.log(open);
-    header.classList.toggle('open')
+  if (header) {
+    const nav = header.querySelector('nav')
+    const button = header.querySelector('button')
+    const initialHeight = header.clientHeight
+    var menuOpen = false
 
-    if (open) {
-      // Remove height on header
-      header.removeAttribute('style')
-      button.innerText = 'Menu'
-      open = false
-    } else {
-      // Add height style on header
-      header.style.height = initialHeight + nav.clientHeight + 'px'
-      button.innerText = 'Sluiten'
-      open = true
+    function toggleMenu() {
+      header.classList.toggle('open')
+
+      if (menuOpen) {
+        // Remove height on header
+        header.removeAttribute('style')
+        button.innerText = 'Menu'
+        menuOpen = false
+      } else {
+        // Add height style on header
+        header.style.height = initialHeight + nav.clientHeight + 'px'
+        button.innerText = 'Sluiten'
+        menuOpen = true
+      }
     }
+
+    button.addEventListener('click', toggleMenu)
   }
-
-  return button.addEventListener('click', toggleMenu)
 })()
-
-export default hdr
