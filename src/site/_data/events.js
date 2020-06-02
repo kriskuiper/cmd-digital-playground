03ed8fe6f8cce6f61a19f28d095cfcd2be442332
Kris <kriskuiper74@gmail.com>
2020-06-02 09:54:38 +0200

    feat(events): get events from Storyblok

---
+++ src/site/_data/events.js
@@ -0,0 +1,19 @@
+const Storyblok = require('../../lib/storyblok-instance')
+
+module.exports = async () => {
+  const env = process.env.ELEVENTY_ENV
+  const version = env === 'production' ? 'published' : 'draft'
+
+  try {
+    const result = await Storyblok.get('cdn/stories', { version, starts_with: 'events' })
+    const events = result.data.stories
+
+    return events
+  } catch(error) {
+    if (process.env.ELEVENTY_ENV === 'development') {
+      console.error('Error fetching events: ', error)
+    }
+
+    return []
+  }
+}
