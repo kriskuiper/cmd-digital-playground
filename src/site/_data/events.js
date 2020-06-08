441c66cbdf439d587879b5167efefdb7efd72a84
gijslaarman <gijslaarman@gmail.com>
2020-06-08 16:03:20 +0200

    feat: eventdata is now passed to components that have event in their name

--- src/site/_data/events.js
+++ src/site/_data/events.js
@@ -8,38 +8,19 @@ module.exports = async () => {
     const result = await Storyblok.get('cdn/stories', { version, starts_with: 'events' })
     const events = result.data.stories
 
-    return {
-      overviewPage: getOverviewPageData(events),
-      events: getEvents(events)
-    }
+    return getEvents(events)
   } catch(error) {
     if (process.env.ELEVENTY_ENV === 'development') {
       console.error('Error fetching events: ', error)
     }
 
-    return {
-      overviewPage: null,
-      events: []
-    }
+    return []
   }
-}
-
-function getOverviewPageData(events) {
-  const overviewPageData = events.find(event => {
-    return event.full_slug === 'events/'
-  })
-
-  return overviewPageData.content
 }
 
 function getEvents(events) {
   return events
-    .filter(event => {
-      return event.full_slug !== 'events/'
-    })
     .map(event => {
-      console.log(event)
-
       return {
         ...event.content,
         full_slug: event.full_slug
--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -2,15 +2,18 @@ const getPagesData = require('../../../lib/get-pages-data')
 const getNavigationData = require('../../../lib/get-navigation-data')
 const Storyblok = require('../../lib/storyblok-instance')
 
+const events = require('./events')
+
 module.exports = async () => {
   const env = process.env.ELEVENTY_ENV
   const version = env === 'production' ? 'published' : 'draft'
 
   const result = await Storyblok.get('cdn/stories', { version })
   const { stories = [] } = result.data
+  const eventData = await events() // Because of the way eleventy works, I fetch the event data once more to pass on to components that have events. Because this is the only place where you can async request events.
 
   return {
     navigation: getNavigationData(stories),
-    stories: getPagesData(stories)
+    stories: getPagesData(stories, eventData)
   }
 }
