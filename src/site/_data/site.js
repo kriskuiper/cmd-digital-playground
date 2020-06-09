c65bc8b850d8feec7538a4375853dcfd18c7d2bf
gijslaarman <gijslaarman@gmail.com>
2020-06-09 10:37:46 +0200

    feat: make fetching events simpler

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -1,19 +1,21 @@
 const getPagesData = require('../../../lib/get-pages-data')
 const getNavigationData = require('../../../lib/get-navigation-data')
 const Storyblok = require('../../lib/storyblok-instance')
-
-const events = require('./events')
+const getEvents = require('../../../lib/get-events')
 
 module.exports = async () => {
   const env = process.env.ELEVENTY_ENV
   const version = env === 'production' ? 'published' : 'draft'
 
-  const result = await Storyblok.get('cdn/stories', { version })
-  const { stories = [] } = result.data
-  const eventData = await events() // Because of the way eleventy works, I fetch the event data once more to pass on to components that have events. Because this is the only place where you can async request events.
+  const [pages, events] = await Promise.all([
+    Storyblok.get('cdn/stories', { version }),
+    Storyblok.get('cdn/stories', { starts_with: 'events', version })
+  ])
+
+  const { stories = [] } = pages.data
 
   return {
     navigation: getNavigationData(stories),
-    stories: getPagesData(stories, eventData)
+    stories: getPagesData(stories, getEvents(events.data.stories))
   }
 }
