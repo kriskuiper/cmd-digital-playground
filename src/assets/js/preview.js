e61447c2715a18850e15d8218b6201f91a14b0a5
Kris <kriskuiper74@gmail.com>
2020-06-15 15:32:01 +0200

    feat: show dialog on save and publish

--- src/assets/js/preview.js
+++ src/assets/js/preview.js
@@ -2,24 +2,34 @@ import throttle from 'lodash.throttle'
 import { apiEndpoints } from './lib/constants'
 
 if (storyblok.isInEditor()) {
-  const tenSeconds = 10000
-  const throttledDeployProduction = throttle(deployProduction, tenSeconds, { trailing: false })
-  const throttledDeployPreview = throttle(deployPreview, tenSeconds, { trailing: false })
+  const dialogContent = `
+    <p class="preview-dialog visually-hidden"></p>
+  `
 
-  storyblok.on('published', 'unpublished', throttledDeployProduction)
-  storyblok.on(['change'], throttledDeployPreview)
+  document.body.insertAdjacentHTML('afterbegin', dialogContent)
+
+  const thirtySeconds = 30000
+  const throttledDeployProduction = throttle(deployProduction, thirtySeconds, { trailing: false })
+  const throttledDeployPreview = throttle(deployPreview, thirtySeconds, { trailing: false })
+
+  storyblok.on(['published', 'unpublished'], throttledDeployProduction)
+  storyblok.on('change', throttledDeployPreview)
 }
 
 storyblok.pingEditor(() => {
-  if (storyblok.inEditor()) {
+  if (storyblok.isInEditor()) {
     storyblok.enterEditmode()
   }
 })
 
 function deployProduction() {
+  showDialog('De pagina is succesvol gepubliceerd of ongepubliceerd. Het duurt even voordat de wijzigingen live staan.')
+
   return fetch(apiEndpoints.DEPLOY_PRODUCTION)
     .then(() => {
-      location.reload()
+      setTimeout(() => {
+        location.reload()
+      }, 30000)
     })
     .catch(error => {
       console.error('Failed to deploy production: ', error)
@@ -27,11 +37,22 @@ function deployProduction() {
 }
 
 function deployPreview() {
+  showDialog('Je wijzigingen worden verwerkt... Het duurt 30 seconden voordat de pagina wordt ververst.')
+
   return fetch('/api/deploy-preview')
     .then(() => {
-      location.reload()
+      setTimeout(() => {
+        location.reload()
+      }, 30000)
     })
     .catch(error => {
       console.error('Failed to deploy preview: ', error)
     })
+}
+
+function showDialog(text) {
+  const dialogElement = document.getElementsByClassName('preview-dialog')[0]
+
+  dialogElement.classList.remove('visually-hidden')
+  dialogElement.textContent = text
 }
