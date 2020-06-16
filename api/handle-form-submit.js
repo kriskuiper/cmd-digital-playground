a3e1a6b51e114a9281d475b1244f0cb9ebe28cd1
gijslaarman <gijslaarman@gmail.com>
2020-06-16 10:50:27 +0200

    refactor: handle-form-submit, using node-fetch

--- api/handle-form-submit.js
+++ api/handle-form-submit.js
@@ -1,7 +1,33 @@
 const render = require('../lib/render')
-const axios = require('axios')
+const fetch = require('node-fetch')
 
 module.exports = async (request, response) => {
+  if (request.method !== 'POST') {
+    // If it's not a post request cancel.
+    response.status(500)
+    return renderMessage('error', { title: '404 niet gevonden', text: 'Het ziet er naar uit dat deze pagina niet (meer) bestaat.' })
+  }
+
+  const { body, query } = request // Helper function build in by Vercel/Now
+
+  // Form validation. TODO: handle this with javascript. Saves a request later down the line.
+  if (!body.email) {
+    return renderMessage('error', { title: 'Geen Email opgegeven', text: 'Vul een geldig email-adres in op het formulier.' })
+  }
+
+  // Send out a post request with the body data to the preferred data handler. e.g: Zapier.
+  fetch(query.handler, {
+    body,
+    method: 'POST'
+  })
+  .then(() => {
+    return renderMessage('success', { title: 'Je bent aangemeld voor het evenement!', text: 'See you soon!' })
+  })
+  .catch(() => {
+    return renderMessage('error', { title: 'Er is iets misgegaan', text: 'Probeer het later nog eens.' })
+  })
+
+
   async function renderMessage(status, msg) {
     const html = await render('submit', { status, msg })
 
@@ -12,24 +38,5 @@ module.exports = async (request, response) => {
     }
 
     return response.send(html)
-  }
-  // Since every request to this route is a POST request we have
-  // access to request.body where all fields can be found
-  const { body, query } = request // Helper function build in by Vercel/Now
-
-  // Is it a POST request or a GET? Currently only supports POST requests.
-  if (request.method === 'POST') {
-      if (!body.email) {
-        renderMessage('error', { title: 'Geen Email opgegeven', text: 'Vul een geldig email-adres in op het formulier.' })
-      }
-
-      // Validation successful, do this:
-      const POST_REQUEST = await axios.post(query.handler, { data: body })
-
-      if (POST_REQUEST.status === 200 && POST_REQUEST.data.status === 'success') {
-        renderMessage('succes', { title: 'Je bent aangemeld voor het evenement!', text: 'See you soon!' })
-      }
-  } else {
-    renderMessage('error', { title: '404 niet gevonden', text: 'Het ziet er naar uit dat deze pagina niet (meer) bestaat.' })
   }
 }
